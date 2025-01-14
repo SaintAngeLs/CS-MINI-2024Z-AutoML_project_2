da9f123ee8db7b356adc498a9c5fb7f44dbf2d93
Andrii Voznesenskyi <voznesenskijandrej5@gmail.com>
2025-01-14 20:35:15 +0100

    (#8) resolve issue with dependencies

--- src/FeatureFlex/__init__.py
+++ src/FeatureFlex/__init__.py
@@ -4,12 +4,12 @@ from .evaluation import ModelEvaluator
 from .model_optimizer import ModelOptimizer
 from .feature_selector import EnhancedFeatureSelector
 from .preprocessing import DataPreprocessor
-from .model import Model
+from .model import DeepRecommendationModel
 
 __all__ = [
     "ModelEvaluator",
     "ModelOptimizer",
     "EnhancedFeatureSelector",
     "DataPreprocessor",
-    "Model",
+    "DeepRecommendationModel",
 ]
